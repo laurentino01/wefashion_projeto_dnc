30da689a5e88b0ba279bedfa0ca84c19be5aba84
Gabriel Laurentino <gabriellaurentino@hotmail.com>
2023-10-25 23:12:30 -0300

    fix cta button link

--- js/header.js
+++ js/header.js
@@ -1,8 +1,10 @@
-document.querySelector('.hamburger').addEventListener('click', function() {
-    const menu = document.querySelector('.menu');
-    menu.style.display = menu.style.display === 'none' ? 'flex' : 'none';
+document.querySelector(".hamburger").addEventListener("click", function () {
+  const menu = document.querySelector(".menu");
+  menu.style.display = menu.style.display === "none" ? "flex" : "none";
 });
 
 function matricular() {
-    // Aqui vai o código de matrícula que você tem, ou deixe em branco por enquanto.
+  const url =
+    "https://api.whatsapp.com/send?phone=5512988880301&text=Ol%C3%A1!%20Gostaria%20de%20me%20matricular%20em%20um%20de%20seus%20cursos.%20";
+  window.open(url, "_blank").focus();
 }
